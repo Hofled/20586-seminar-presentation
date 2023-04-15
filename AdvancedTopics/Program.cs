879b5aa0d6cdf32cd3ffcad8106295f838c3a3c6
Hofled <ofri.herold@gmail.com>
2023-04-16 01:39:02 +0300

    Thread example

--- AdvancedTopics/Program.cs
+++ AdvancedTopics/Program.cs
@@ -1,15 +1,37 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
-namespace AdvancedTopics
+class Program
 {
-    class Program
+    static void Main()
     {
-        static void Main(string[] args)
-        {
-        }
+        Console.WriteLine("Starting thread...");
+
+        Thread thread = new Thread(LongRunningOperation);
+        thread.Start();
+
+        Console.WriteLine("Main thread continues...");
+
+        Thread.Sleep(500);
+
+        // Receive input from user while we are waiting for the thread to finish
+        Console.Write("\nPlease insert your name:");
+        var name = Console.ReadLine();
+        Console.WriteLine($"Thank you for waiting {name}, the computation will finish shortly...");
+
+        // Wait for the thread to complete before exiting the program
+        thread.Join();
+
+        Console.WriteLine("Program finished.");
+
+        Console.ReadLine();
+    }
+
+    static void LongRunningOperation()
+    {
+        Console.WriteLine("Long running operation started...");
+        // Sleep for 10 seconds
+        Thread.Sleep(10000);
+        Console.WriteLine("Long running operation completed.");
     }
 }

868940274784545bd365ad7584c04ab1e53276b4
Hofled <ofri.herold@gmail.com>
2023-04-16 01:44:14 +0300

    async await task example

--- AdvancedTopics/Program.cs
+++ AdvancedTopics/Program.cs
@@ -1,15 +1,27 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
-namespace AdvancedTopics
+class Program
 {
-    class Program
+    // Note that the main function is now returning a 'Task' and is marked as 'async'
+    static async Task Main()
     {
-        static void Main(string[] args)
-        {
-        }
+        Console.WriteLine("Starting async operation...");
+
+        int result = await Task.Run(() => LongRunningOperation());
+
+        Console.WriteLine($"Async operation completed with result: {result}");
+
+        Console.WriteLine("Program finished.");
+
+        Console.ReadLine();
+    }
+
+    static int LongRunningOperation()
+    {
+        Console.WriteLine("Long running operation started...");
+        Task.Delay(3000).Wait();
+        Console.WriteLine("Long running operation completed.");
+        return 42;
     }
 }
